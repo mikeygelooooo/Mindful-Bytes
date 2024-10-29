f3523a6d8b182556d84a1f824051e1dfaa1c2363
mikeygelooooo <mikeygelo7@gmail.com>
2024-10-29 15:08:19 +0800

    Custom Login and Signup Forms

--- User/forms.py
+++ User/forms.py
@@ -3,6 +3,46 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=254,
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Email'}
+        )
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Username'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Password'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Confirm Password'
+        })
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('This email address is already in use.')
+        return email
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Username'}
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Password'}
+        )
+    )
--- User/views.py
+++ User/views.py
@@ -1,45 +1,54 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login as auth_login
-from .forms import SignupForm
-from django.urls import reverse
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+from .forms import SignupForm, LoginForm
+
 
 # Create your views here.
 def login(request):
     if request.user.is_authenticated:
-        return redirect(reverse('home'))
-    
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            auth_login(request, user)
-            return redirect('/')
-    else:
-        form = AuthenticationForm()
+        return redirect("home")
 
-    return render(request, 'User/login.html', {'form' : form})
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                auth_login(request, user)
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password.")
+    else:
+        form = LoginForm()
+
+    return render(request, "User/login.html", {"form": form})
+
 
 def signup(request):
     if request.user.is_authenticated:
-        return redirect(reverse('home'))
-    
-    if request.method == 'POST':
+        return redirect("home")
+
+    if request.method == "POST":
         form = SignupForm(request.POST)
+
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect('/')
+            messages.success(request, "Account created successfully!")
+            return redirect("home")
     else:
         form = SignupForm()
 
-    return render(request, 'User/signup.html', {'form' : form})
+    return render(request, "User/signup.html", {"form": form})
+
 
 def profile(request):
     if request.user.is_authenticated:
         username = request.user.username
     else:
-        return redirect(reverse('login'))
+        return redirect("login")
 
-    return render(request, 'User/profile.html', {'username': username})
+    return render(request, "User/profile.html", {"username": username})
